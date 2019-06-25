4ac3ac576d9ac73d615f80b7e8f5af53a25a53a1
cheungkinwong <cheungkin.wong@gmail.com>
2019-06-25 09:36:08 +0200

    scaled and cropped images

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
 const memory = document.getElementById("memory");
-const container = document.getElementById("cardContainer");
-const card = document.getElementById("card");
+const card = document.querySelectorAll(".card");
 
-container.addEventListener("click", () =>
-  document.querySelector(".card").classList.toggle("flip")
-);
+card.forEach(cardFlip => {
+  cardFlip.addEventListener("click", flip);
+});
 
-// let random = () => Math.floor(Math.random() * 19);
-
-// function deal() {
-//   let newCard = document.createElement("div");
-//   newCard.classList.add("card");
-//   container.appendChild(newCard);
-// }
-
-// $("#button").onClick(() => $("#target_element").addClass("animate_class_name"));
+function flip() {
+  this.classList.toggle("flip");
+  console.log(this);
+}
