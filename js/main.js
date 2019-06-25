1216f8f546ced6906a0c1fe876b879b4d5dece7d
cheungkinwong <cheungkin.wong@gmail.com>
2019-06-25 12:00:07 +0200

    add music and twinkling background

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
 const memory = document.getElementById("memory");
 const card = document.querySelectorAll(".card");
+const music = document.getElementById("dearlyBeloved");
+let isFlipped = false;
 
 card.forEach(cardFlip => {
   cardFlip.addEventListener("click", flip);
 });
 
+playMusic();
+dealCards();
+function dealCards() {
+  card.forEach(card => {
+    let shuffle = Math.floor(Math.random() * 11);
+    card.style.order = shuffle;
+  });
+}
+
 function flip() {
   this.classList.toggle("flip");
-  console.log(this);
+  if (isFlipped === false) {
+    isFlipped = true;
+    console.log(this);
+  } else {
+    isFlipped = false;
+  }
+}
+
+function playMusic() {
+  console.log("lol");
+  music.play();
+  music.loop = true;
 }
