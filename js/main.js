52919eaacf07f19809cea89993c4fbececd9dbb3
cheungkinwong <cheungkin.wong@gmail.com>
2019-06-28 10:07:53 +0200

    added comments js

--- js/main.js
+++ js/main.js
@@ -8,8 +8,7 @@ const p2 = document.getElementById("p2");
 const playGuide = document.querySelector(".playGuide");
 const cardFace = document.getElementById("cardFace");
 const result = document.getElementById("result");
-const newCard = shuffle(cardArray);
-console.log(newCard);
+let newCard = shuffle(cardArray);
 
 let p1Score = document.getElementById("p1score");
 let p2Score = document.getElementById("p2score");
@@ -24,6 +23,11 @@ let wait = false;
 let index = 0;
 let firstCard, secondCard;
 
+//start game
+playMusic();
+newGame();
+
+//fade out playGuide
 playGuide.addEventListener("click", fade);
 function fade() {
   playGuide.classList.add("fade");
@@ -32,20 +36,21 @@ function fade() {
   }, 1500);
 }
 
+//cards shuffled and flippable
+function newGame() {
+  addFlip();
+  shuffle(cardArray);
+  dealCard();
+}
+
+//add click on cards
 function addFlip() {
   card.forEach(cardFlip => {
     cardFlip.addEventListener("click", flip);
   });
 }
 
-start();
-
-function start() {
-  addFlip();
-  playMusic();
-  dealCard();
-}
-
+//shuffle my cards
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -54,12 +59,14 @@ function shuffle(array) {
   return array;
 }
 
+//append the cards to cardContainer
 function dealCard() {
   for (let i = 0; i < 10; i++) {
     cardContainer.appendChild(newCard[i]);
   }
 }
 
+//switch player turn
 function changeTurn() {
   p1.classList.toggle("selected");
   p2.classList.toggle("selected");
@@ -70,22 +77,30 @@ function changeTurn() {
   }
 }
 
+//flip the card
 function flip() {
+  //make you wait to click again
   if (wait) return;
+  //can't click the same card twice
   if (this === firstCard) return;
+  //add css flip to card
   this.classList.add("flip");
+  //first card
   if (!newTurn) {
     newTurn = true;
     firstCard = this;
   } else {
+    //second card
     newTurn = false;
     secondCard = this;
     compare();
   }
 }
 
+//compare the two cards
 function compare() {
   if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    //add to score if correct
     if (player1 === true) {
       score1 += 1;
       pairsFound += 1;
@@ -95,6 +110,7 @@ function compare() {
       pairsFound += 1;
       p2Score.innerHTML = score2;
     }
+    //determine winner
     setTimeout(() => {
       if (pairsFound === 5) {
         if (score1 > score2) {
@@ -104,11 +120,12 @@ function compare() {
         }
       }
     }, 1500);
-
+    //can't click on found pairs
     firstCard.removeEventListener("click", flip);
     secondCard.removeEventListener("click", flip);
     clearBoard();
   } else {
+    //flips back if it's not a pair
     wait = true;
     setTimeout(() => {
       firstCard.classList.remove("flip");
@@ -120,12 +137,14 @@ function compare() {
   }
 }
 
+//play music in loop
 function playMusic() {
   music.loop = true;
   music.play();
   music.volume = 0.2;
 }
 
+//reset board
 function clearBoard() {
   newTurn = false;
   wait = false;
@@ -133,8 +152,8 @@ function clearBoard() {
   secondCard = null;
 }
 
+//restart game on click
 result.addEventListener("click", reset);
-
 function reset() {
   clearBoard();
   score1 = 0;
@@ -143,15 +162,19 @@ function reset() {
   p1Score.innerHTML = score1;
   p2Score.innerHTML = score2;
   result.innerHTML = "";
-
+  //flips back all card and start newGame
   card.forEach(cardReset => {
     cardReset.classList.remove("flip");
   });
-  setTimeout(addFlip, dealCards, 1000);
+  setTimeout(newGame, 1000);
+
+  //make sure at reset it starts with player 1
+  if (player1) return;
+  changeTurn();
 }
 
+//change style on click
 cardFace.addEventListener("click", toggleStyle);
-
 function toggleStyle() {
   reset();
   setTimeout(style, 1500);
@@ -165,13 +188,13 @@ function toggleStyle() {
   }
 }
 
+//replace images
 function style() {
   const card1 = document.querySelectorAll(".d1");
   const card2 = document.querySelectorAll(".d2");
   const card3 = document.querySelectorAll(".d3");
   const card4 = document.querySelectorAll(".d4");
   const card5 = document.querySelectorAll(".d5");
-
   if (!dream) {
     card1.forEach(thisCard => (thisCard.src = "../img/n1.jpg"));
     card2.forEach(thisCard => (thisCard.src = "../img/n2.jpg"));
@@ -186,6 +209,8 @@ function style() {
     card5.forEach(thisCard => (thisCard.src = "../img/5.jpg"));
   }
 }
+
+//keyboard control
 document.addEventListener("keydown", move);
 function move(event) {
   switch (event.keyCode) {
@@ -207,12 +232,12 @@ function move(event) {
   }
 }
 
+//movement of selector using keyboard
 function left() {
   if (index === 0 || index === 5) return;
   newCard[index].children[1].classList.remove("selectedCard");
   index -= 1;
   newCard[index].children[1].classList.add("selectedCard");
-  console.log(newCard[index].children[1], "lol");
 }
 
 function right() {
